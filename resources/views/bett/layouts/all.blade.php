01c71e533f5d7d0d203856cb87c7e154e8a693a5
alik <pansikl@mail.ru>
2021-09-18 18:05:52 +0300

    noscript перенес в body

--- resources/views/bett/layouts/all.blade.php
+++ resources/views/bett/layouts/all.blade.php
@@ -6,6 +6,10 @@
   <body class="{{ isset($bodyImage) ? 'page-'.$bodyImage : 'Default' }} js-focus-visible" style="">
     @include(env('THEME').'.layouts.header')
 
+    <!-- Yandex.Metrika counter -->
+    <noscript><div><img src="https://mc.yandex.ru/watch/85342297" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
 
 <script>
 	(function () {
--- resources/views/bett/layouts/head.blade.php
+++ resources/views/bett/layouts/head.blade.php
@@ -15,6 +15,7 @@ __wm.init("https://web.archive.org/web");
 __wm.wombat("https://betfaq.ru/","20190828062649","https://web.archive.org/","web","/_static/",
     "1566973609");
 </script> -->
+
 <!-- Yandex.Metrika informer -->
 <a href="https://metrika.yandex.ru/stat/?id=85342297&amp;from=informer"
 target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/85342297/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
@@ -23,19 +24,20 @@ style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" tit
 
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(85342297, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
+ym(85342297, "init", {
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true,
+  webvisor:true
+});
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/85342297" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- <noscript><div><img src="https://mc.yandex.ru/watch/85342297" style="position:absolute; left:-9999px;" alt="" /></div></noscript> -->
 <!-- /Yandex.Metrika counter -->
+
 <link rel="stylesheet" type="text/css" href="{{ asset(env('THEME')) }}/css/banner-styles.css">
 <link rel="stylesheet" type="text/css" href="{{ asset(env('THEME')) }}/css/iconochive.css">
 <!-- End Wayback Rewrite JS Include -->
